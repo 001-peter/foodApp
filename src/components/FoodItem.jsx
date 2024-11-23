2ee5a203e3cbd1a571a3e959c4f9c7995529a9ca
001-peter <onipeter2003@gmail.com>
2024-11-23 17:57:06 +0100

    updates

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -2,26 +2,6 @@ import styles from "./foodItem.module.css";
 export default function FoodItem({ food, setFoodId }) {
   return (
     <div>
-      {/* <div className="card" style="width: 18rem;">
-      <img className={styles.itemImage} src={food.image} alt="" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button href="#" class="btn btn-primary"
-           onClick={() => {
-            console.log(food.id);
-            setFoodId(food.id);
-          }}
-          className={styles.itemButton}
-          view recipe >
-           </>
-         </div>
-      </div> */}
-
-      
       <div className={styles.itemContainer}>
       <img className={styles.itemImage} src={food.image} alt="" />
         <div className={styles.itemContent}>
